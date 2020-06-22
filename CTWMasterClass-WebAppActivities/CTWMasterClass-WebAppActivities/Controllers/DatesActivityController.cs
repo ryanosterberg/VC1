f8315cdb609b848d57913c90f9a9b35c746e5864
Jacob <Jacob.r.osterberg@gmail.com>
2020-06-22 13:48:25 -0500

    beginnings of dates

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/DatesActivityController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class DatesActivityController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: DateTime.Now);
+        }
+        [HttpPost]
+        public ActionResult DateMagic(DateTime inputBirthday)
+        {
+            return View(viewName: "Index", model: inputBirthday);
+        }
+        public ActionResult Presidential()
+        {
+            return View(viewName: "Index", model: DateTime.Now);
+        }
+        [HttpPost]
+        public ActionResult SayPresidential(DateTime inputBirthday)
+        {
+            return View(viewName: "Index", model: inputBirthday);
+        }
+    }
+}
+
--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/SquareANumberController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/SquareANumberController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 
 namespace CTWMasterClass_WebAppActivities.Controllers
 {
@@ -10,13 +13,39 @@ namespace CTWMasterClass_WebAppActivities.Controllers
     {
         public ActionResult Index()
         {
-            return View(viewName: "Index", model: 0);
+            return View(viewName: "Index", model: 0.5);
         }
         [HttpPost]
-        public ActionResult SaySquare(int inputNumber)
+        public ActionResult SaySquare(double inputNumber)
         {
-            return View(viewName: "Index", model: (inputNumber * inputNumber));
+            return View(viewName: "Index", model: (Math.Sqrt(inputNumber)));
         }
+        public ActionResult MagicNumber()
 
+        {
+            return View(viewName: "MagicNumber", model: 0);
+        }
+        [HttpPost]
+        public ActionResult SayMagicNumber(int inputNumber1, int inputNumber2)
+        {
+            int magicNum = 0;
+            Random newrand = new Random();
+            if (inputNumber1 == inputNumber2)
+            {
+                return View(viewName: "MagicNumber", model: (-999));
+
+            }
+            if (inputNumber1++ < inputNumber2)
+            {
+                magicNum = newrand.Next(inputNumber1++, inputNumber2);
+                return View(viewName: "MagicNumber", model: (magicNum));
+            }
+            if (inputNumber2++ < inputNumber1)
+            {
+                magicNum = newrand.Next(inputNumber2++, inputNumber1);
+                return View(viewName: "MagicNumber", model: (magicNum));
+            }
+            return View(viewName: "MagicNumber", model: (-999));
+        }
     }
 }
