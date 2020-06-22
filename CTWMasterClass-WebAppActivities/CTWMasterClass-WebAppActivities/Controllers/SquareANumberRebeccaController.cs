a18652fff810c560894414f1e18e69fa6c70ef6f
Rebecca Gilpatric <rebeccagilpatric@gmail.com>
2020-06-22 13:21:55 -0500

    Added magic number

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/SquareANumberRebeccaController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/SquareANumberRebeccaController.cs
@@ -13,9 +13,31 @@ namespace CTWMasterClass_WebAppActivities.Controllers
             return View(viewName: "Index", model: 0);
         }
         [HttpPost]
-        public ActionResult SaySquare(int inputNumber)
+        public ActionResult SaySquare(double inputNumber)
         {
-            return View(viewName: "Index", model: (inputNumber * inputNumber));
+            return View(viewName: "Index", model: (Math.Sqrt(inputNumber)));
+        }
+
+        public ActionResult Index2()
+        {
+            return View(viewName: "Index2", model: 0);
+        }
+        public ActionResult MagicNumber(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+            {
+                int temp = firstNumber;
+                firstNumber = secondNumber;
+                secondNumber = temp;
+            }
+            Random rand = new Random();
+            int magicNumber;
+            try{
+                magicNumber = rand.Next(firstNumber, secondNumber + 1);
+            }
+            catch { magicNumber = -999; }
+            
+            return View(viewName: "Index2", model: magicNumber);
         }
 
     }
