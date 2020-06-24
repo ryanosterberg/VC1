da2a791ba28d20f5b6328d9c16175a7772560e86
Mr O <ryan.p.osterberg@gmail.com>
2020-06-24 09:52:39 -0500

    lecture

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/SqureANumberController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class SqureANumberController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: -10.99);
+        }
+        [HttpPost]
+        public ActionResult SaySquare(double inputNumber)
+        {
+            return View(viewName: "Index", model: (Math.Sqrt(inputNumber)));
+        }
+
+    }
+}
---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/StringActivityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class StringActivityController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: "All sdhlsdlsdjlsfklstarts here.");
+        }
+        [HttpPost]
+        public ActionResult SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello world " + inputString);
+        }
+
+    }
+}
